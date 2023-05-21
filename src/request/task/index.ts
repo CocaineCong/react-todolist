e4acd47fbf06437153e0872cf2323f3217f8189e
FanOne <294350394@qq.com>
2023-05-21 19:20:03 +0800

    feat:task request

--- src/request/task/index.ts
+++ src/request/task/index.ts
@@ -1,42 +1,76 @@
 import _ from 'lodash';
 import instance from "../index";
 
-const taskBaseUrl = "/api/v1/task/"
+const taskBaseUrl = "/api/v1/"
 
-export async function listProduct (body?: API.ProductListReq, options?: { [key: string]: any }) {
+// 获取所有的task
+export async function listTask (body?: API.ListTaskReq, options?: { [key: string]: any }) {
     let params = new FormData();
     _.forIn(body, function (value, key) {
       return params.append(key, value);
     })
-    return instance<API.ProductListResp>(productBaseUrl+`list`, {
+    return instance<API.CommonResp>(taskBaseUrl+`task_list`, {
         method: 'GET',
         params:params,
         ...(options || {}),
     });
 }
 
-export async function getProduct (body?:any, options?: { [key: string]: any }) {
-    return instance<API.CommonResp>(productBaseUrl+`show`, {
+// 获取某一个task
+export async function getTask (body?:any, options?: { [key: string]: any }) {
+    return instance<API.CommonResp>(taskBaseUrl+`task_show`, {
         method: 'GET',
         params:body,
         ...(options || {}),
     });
 }
 
-export async function getProductImg (body?:any, options?: { [key: string]: any }) {
-    return instance<API.CommonResp>(productBaseUrl+`imgs/list`, {
-        method: 'GET',
-        params:body,
-        ...(options || {}),
-    });
-}
-
-export async function createProduct(body:API.ProductCreateReq,options?:{[key:string]:any}){
+// 创建一个task
+export async function createTask(body:API.CreateTaskReq, options?:{[key:string]:any}){
     let params = new FormData();
     _.forIn(body,function(value,key){
-        return params.append(key,value);
+        return params.append(key, value);
     })
-    return instance<API.CommonResp>(productBaseUrl+`create`,{
+    return instance<API.CommonResp>(taskBaseUrl+`task_create`,{
+        method:'POST',
+        data:params,
+        ...(options||{}),
+    });
+}
+
+// 创建一个task
+export async function updateTask(body:API.UpdateTaskReq, options?:{[key:string]:any}){
+    let params = new FormData();
+    _.forIn(body,function(value,key){
+        return params.append(key, value);
+    })
+    return instance<API.CommonResp>(taskBaseUrl+`task_update`,{
+        method:'POST',
+        data:params,
+        ...(options||{}),
+    });
+}
+
+// 搜索task
+export async function searchTask(body:API.SearchTaskReq, options?:{[key:string]:any}){
+    let params = new FormData();
+    _.forIn(body,function(value,key){
+        return params.append(key, value);
+    })
+    return instance<API.CommonResp>(taskBaseUrl+`task_search`,{
+        method:'POST',
+        data:params,
+        ...(options||{}),
+    });
+}
+
+// 删除task
+export async function deleteTask(body:any, options?:{[key:string]:any}){
+    let params = new FormData();
+    _.forIn(body,function(value,key){
+        return params.append(key, value);
+    })
+    return instance<API.CommonResp>(taskBaseUrl+`task_delete`,{
         method:'POST',
         data:params,
         ...(options||{}),
